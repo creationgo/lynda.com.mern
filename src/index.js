957d6127e39e549d672b377c502cffd14046a9fa
nicolasjn <nicolasjuarezn@gmail.com>
2017-07-10 12:55:27 -0300

    First Commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+// import axios from 'axios';
+
+import App from './components/App';
+
+ReactDOM.render(
+  <App initialData={window.initialData} />,
+  document.getElementById('root')
+);
